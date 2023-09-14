7a018e3f59ebc3b8b4b948d51a694475014c9e25
kent108 <quentin.cutrim@gmail.com>
2023-09-14 09:55:47 +0200

    1 - Premier commit

--- js/disclosure.js
+++ js/disclosure.js
@@ -3,6 +3,14 @@ let disclosure = document.getElementById("disclosureWindow");
 
 document.addEventListener("DOMContentLoaded", () => {
   discButton.addEventListener("click", (e) => {
-    disclosure.style.display = "block";
+    if (disclosure.style.display === "block") {
+      // Si la divulgation est actuellement ouverte, la fermer
+      disclosure.style.display = "none";
+      discButton.setAttribute("aria-expanded", "false");
+    } else {
+      // Sinon, l'ouvrir
+      disclosure.style.display = "block";
+      discButton.setAttribute("aria-expanded", "true");
+    }
   });
 });
--- js/modal.js
+++ js/modal.js
@@ -7,15 +7,52 @@ document.addEventListener("DOMContentLoaded", () => {
   );
   var firstFocusable = focusable[0];
   var lastFocusable = focusable[focusable.length - 1];
-  console.log(focusable);
+
   openButton.addEventListener("click", (event) => {
     event.preventDefault();
-    modal.style.display = "flex";
+    openModal();
   });
 
-  firstFocusable.addEventListener("click", (event) => {
-    event.preventDefault();
+  function openModal() {
+    modal.style.display = "flex";
+    // Donner le focus au premier élément focusable dans la modal
+    firstFocusable.focus();
+    // Désactiver la navigation au clavier en dehors de la modal
+    document.addEventListener("keydown", trapFocus);
+    document.addEventListener("keydown", function (event) {
+      if (event.key === "Escape") {
+        closeModal();
+      }
+    });
+
+  }
+
+  function trapFocus(event) {
+    if (event.key === "Tab") {
+      if (event.shiftKey) {
+        // Si la touche Majuscule est enfoncée, revenir au dernier élément focusable
+        if (document.activeElement === firstFocusable) {
+          event.preventDefault();
+          lastFocusable.focus();
+        }
+      } else {
+        // Sinon, avancer vers le premier élément focusable
+        if (document.activeElement === lastFocusable) {
+          event.preventDefault();
+          firstFocusable.focus();
+        }
+      }
+    }
+  }
+
+  function closeModal() {
     modal.style.display = "none";
     openButton.focus();
-  });
+    // Réactiver la navigation au clavier en dehors de la modal
+    document.removeEventListener("keydown", trapFocus);
+  }
+
+  // Gérer la fermeture de la modal lorsque le bouton X est cliqué
+  let closeButton = modal.querySelector("button");
+  closeButton.addEventListener("click", closeModal);
 });
